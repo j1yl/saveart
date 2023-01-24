68fe5a2e2660ede35d7bfab1e9b362cacfac749b
Joe Lee <lefodev@gmail.com>
2023-01-23 17:53:03 -0800

    Add client code

---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,13 @@
+<main class="px-4 flex flex-col h-screen justify-center">
+	<div class="flex flex-col gap-2">
+		<h1 class="font-semibold text-4xl">create through generative ai and post to our board</h1>
+		<h3>
+			at saveart, our goal is to create a space for those who want to test out the DALL-E model from
+			openAI
+		</h3>
+	</div>
+	<div class="flex gap-2 mt-4">
+		<a href="/get-started" class="px-4 py-1 text-lg text-black bg-white rounded-xl">start now</a>
+		<a href="/about" class="px-4 py-1 text-lg text-black bg-white rounded-xl">learn more</a>
+	</div>
+</main>
---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,11 @@
+<nav class="p-4 mx-auto flex justify-between">
+	<div>
+		<a href="/" class="font-bold text-2xl">saveart</a>
+	</div>
+	<div class="flex gap-4 items-center">
+		<a href="/" class="font-semibold underline text-sm">home</a>
+		<a href="/about" class="font-semibold underline text-sm">about</a>
+		<a href="/posts" class="font-semibold underline text-sm">posts</a>
+		<a href="/login" class="font-semibold underline text-sm">login</a>
+	</div>
+</nav>
